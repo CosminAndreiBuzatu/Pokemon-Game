be10f4bb72ea924937769740eaac1cc01a382b18
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-10-04 16:22:20 +0100

    connected the api to python and printed out the list of names for each pokemon

--- fetchApi.py
+++ fetchApi.py
@@ -2,4 +2,20 @@
 # https://pokeapi.co/
 import requests
 
+r = requests.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
+print(r.text)
+response = r.json()
 
+Items = response["results"]
+for pokemon in Items:
+        print(pokemon["name"])
+#     theFirstObject = departures[busNumber][0]
+#     # for each bus object in the response
+#     print(busNumber)
+
+# Create a database which is able to store the following information about a Pokemon:
+# - Name
+# - Artwork image (as URL)
+# - Attack value
+# - Defense value
+# - Types
