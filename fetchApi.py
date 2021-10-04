9dc4daa7ed63fbc93c187d3f2d0a7a3ac69e4a77
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-10-04 15:02:57 +0100

    Added poetry, flask and requests

--- fetchApi.py
+++ fetchApi.py
@@ -2,9 +2,25 @@
 # https://pokeapi.co/
 import requests
 
-r = requests.get('https://pokeapi.co/api/v2/pokemon/ditto')
+r = requests.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
+#
+# response = r.json()
+# r.text
 
-response = r.json()
-r.text
 
+# items = response[""]
+#
+# for pokemon in items:
+#     theFirstObject = departures[busNumber][0]
+#     print()
 
+# if __name__ == "__main__":
+#     main()
+
+# Store the following:
+# For each Pokemon:
+# - Name
+# - Artwork image (as URL)
+# - Attack value
+# - Defense value
+# - Types
