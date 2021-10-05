0556df7f5935eae9c584606a06a8250b94bfa7ce
rthethi <rajpal.thethi@altran.com>
2021-10-05 15:37:11 +0100

    flask methods for mainpage, pokedex, downloadpokedex

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Constant file for non changing variables.
+
+bulbasaur_dict = {
+        "name": "Bulbasaur",
+        "artwork": "url",
+        "attack": 40,
+        "defence": 40,
+        "type1": "Grass",
+        "type2": "Poison"
+    }
